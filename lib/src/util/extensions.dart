44859fed7a3782f84fb187eaee9ac4dc508203a5
AoiHosizora <a970335605@hotmail.com>
2020-12-09 23:59:17 +0800

    feat(extension): update extension comments and add BoolExtension

--- lib/src/util/extensions.dart
+++ lib/src/util/extensions.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 /// [StateExtension] is a helper extension for [State].
 extension StateExtension<T extends StatefulWidget> on State<T> {
+  /// Equals to call `if (mounted) setState(() {});`
   void mountedSetState(void Function() func) {
     func?.call();
     // ignore: invalid_use_of_protected_member
@@ -11,9 +12,38 @@ extension StateExtension<T extends StatefulWidget> on State<T> {
 
 /// [ObjectExtension] is a helper extension for [Object].
 extension ObjectExtension on Object {
+  /// [doIf] can use lambda for `if` statement.
   void doIf(bool condition, void Function() func) {
     if (condition) {
       func?.call();
     }
+  }
+
+  /// [doIfElse] can use lambda for `if-else` statement.
+  void doIfElse(bool condition, void Function() ifFunc, void Function() elseFunc) {
+    if (condition) {
+      ifFunc?.call();
+    } else {
+      elseFunc?.call();
+    }
+  }
+}
+
+/// [BoolExtension] is a helper extension for [bool].
+extension BoolExtension on bool {
+  /// Returns value if condition is true.
+  T returnIfTrue<T>(T Function() func) {
+    if (this) {
+      return func?.call();
+    }
+    return null;
+  }
+
+  /// Returns value if condition is false.
+  T returnIfFalse<T>(T Function() func) {
+    if (this) {
+      return func?.call();
+    }
+    return null;
   }
 }
